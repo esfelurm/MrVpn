27cc5571562263c30f920fb8dfae694570937135
EsfeLurM <104654028+esfelurm@users.noreply.github.com>
2023-12-21 09:18:15 +0330

    Create RandomBanner.py

---
+++ RandomBanner.py
@@ -0,0 +1,106 @@
+import random
+from colorama import init
+
+rd, gn, lgn, yw, lrd, be, pe = '\033[00;31m', '\033[00;32m', '\033[01;32m', '\033[01;33m', '\033[01;31m', '\033[00;34m', '\033[01;35m'
+cn = '\033[00;36m'
+colors0 = [rd, gn, lgn, yw, lrd, be, pe, cn]
+colors = random.choice(colors0)
+color = random.choice(colors0)
+init()
+class RandomBanner:
+    def __init__(self):
+        self.banner_list = [f"""{colors}
+  __    __     __ __    -  _     _     __ __      __   __   
+ /_/\  /\_\   /_/\__/\    /_/\ /\_\   /_/\__/\   /_/\ /\_\  
+ ) ) \/ ( (   ) ) ) ) )   ) ) ) ( (   ) ) ) ) )  ) ) \ ( (  
+/_/ \  / \_\ /_/ /_/_/   /_/ / \ \_\ /_/ /_/ /  /_/   \ \_\ 
+\ \ \\// / / \ \ \ \ \   \ \ \_/ / / \ \ \_\/   \ \ \   / / 
+ )_) )( (_(   )_) ) \ \   \ \   / /   )_) )      )_) \ (_(  
+ \_\/  \/_/   \_\/ \_\/    \_\_/_/    \_\/       \_\/ \/_/  
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm                                                           
+""", f"""{colors}
+ _      ____        _     ____  _     
+/ \__/|/  __\      / \ |\/  __\/ \  /|
+| |\/|||  \/|_____ | | //|  \/|| |\ ||
+| |  |||    /\____\| \// |  __/| | \||
+\_/  \|\_/\_\      \__/  \_/   \_/  \|
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm
+""", f"""{colors}
+__  __               __      __              
+|  \/  |              \ \    / /              
+| \  / | _ __  ______  \ \  / /  _ __   _ __  
+| |\/| || '__||______|  \ \/ /  | '_ \ | '_ \ 
+| |  | || |              \  /   | |_) || | | |
+|_|  |_||_|               \/    | .__/ |_| |_|
+                                | |           
+                                |_|           
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm
+""",f"""{colors}
+     e    e                  Y88b      /                    
+    d8b  d8b     888-~\       Y88b    /  888-~88e  888-~88e 
+   d888bdY88b    888    ____   Y88b  /   888  888b 888  888 
+  / Y88Y Y888b   888            Y888/    888  8888 888  888 
+ /   YY   Y888b  888             Y8/     888  888P 888  888 
+/          Y888b 888              Y      888-_88"  888  888 
+                                         888                
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm
+""",f"""{colors}
+___ ___                ___ ___               
+|   Y   |.----. ______ |   Y   |.-----..-----.
+|.      ||   _||______||.  |   ||  _  ||     |
+|. \_/  ||__|          |.  |   ||   __||__|__|
+|:  |   |              |:  1   ||__|          
+|::.|:. |               \:.. ./               
+`--- ---'                `---'       
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm
+""",f"""{colors}
+  __  __      ____             __     __     ____      _   _     
+U|' \/ '|u U |  _"\ u          \ \   /"/u  U|  _"\ u  | \ |"|    
+\| |\/| |/  \| |_) |/   U  u    \ \ / //   \| |_) |/ <|  \| |>   
+ | |  | |    |  _ <     /___\   /\ V /_,-.  |  __/   U| |\  |u   
+ |_|  |_|    |_| \_\   |__"__| U  \_/-(_/   |_|       |_| \_|    
+<<,-,,-.     //   \\_            //         ||>>_     ||   \\,-. 
+ (./  \.)   (__)  (__)          (__)       (__)__)    (_")  (_/  
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm
+""",f"""{colors}
+   *                                          
+ (  `                                         
+ )\))(    (           (   (                   
+((_)()\   )(    ___   )\  )\   `  )     (     
+(_()((_) (()\  |___| ((_)((_)  /(/(     )\ )  
+|  \/  |  ((_)       \ \ / /  ((_)_\   _(_/(  
+| |\/| | | '_|        \ V /   | '_ \) | ' \)) 
+|_|  |_| |_|           \_/    | .__/  |_||_|  
+                              |_|            
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm 
+""",f"""{colors}
+ __   __  ______          __   __  _______  __    _ 
+|  |_|  ||    _ |        |  | |  ||       ||  |  | |
+|       ||   | ||  ____  |  |_|  ||    _  ||   |_| |
+|       ||   |_|| |____| |       ||   |_| ||       |
+|       ||    __ |       |       ||    ___||  _    |
+| ||_|| ||   |  ||        |     | |   |    | | |   |
+|_|   |_||___|  ||         |___|  |___|    |_|  |__|
+
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm
+""",f"""{colors}
+.___  ___. .______              ____    ____ .______   .__   __. 
+|   \/   | |   _  \             \   \  /   / |   _  \  |  \ |  | 
+|  \  /  | |  |_)  |     ______  \   \/   /  |  |_)  | |   \|  | 
+|  |\/|  | |      /     |______|  \      /   |   ___/  |  . `  | 
+|  |  |  | |  |\  \----.           \    /    |  |      |  |\   | 
+|__|  |__| | _| `._____|            \__/     | _|      |__| \__|     {color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm
+""",f"""{colors}
+   __  __                      _  _                      
+  F  \/  ]    _ ___           FJ  L]   _ ___     _ ___   
+ J |\__/| L  J '__ ", ______ J |  | L J '__ J   J '__ J  
+ | |`--'| |  | |__|-J|______|J J  F L | |--| |  | |__| | 
+ F L    J J  F L  `-'L______JJ\ \/ /F F L__J J  F L  J J 
+J__L    J__LJ__L              \\__// J  _____/LJ__L  J__L
+|__L    J__||__L               \__/  |_J_____F |__L  J__|
+                                     L_J              
+{color}\n\nMr. vpn tool, a tool for cracking VPN accounts \n\nGit & Tg : @esfelurm                                        
+"""]
+
+    def random_banner(self):
+        return random.choice(self.banner_list)
